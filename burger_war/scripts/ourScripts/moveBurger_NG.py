a44d47ff88cc1517814d2f532db503306bb4a621
TsukasaTaniguchi <ttadgjmptw22@gmail.com>
2019-08-28 20:02:13 +0900

    araichnge

---
+++ burger_war/scripts/ourScripts/moveBurger_NG.py
@@ -0,0 +1,84 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+import rospy
+import tf
+from std_msgs.msg import String
+from std_msgs.msg import Int8
+from geometry_msgs.msg import Quaternion
+from geometry_msgs.msg import PoseStamped
+from actionlib_msgs.msg import GoalStatusArray
+from actionlib_msgs.msg import GoalStatus
+import math
+import numpy as np
+from nav_msgs.msg import OccupancyGrid
+from geometry_msgs.msg import Pose2D
+
+class OnigiriRun(object):
+
+	def __init__(self):
+		self.my_bot_id = ""
+		self.movebase_status = 3
+		#ROS publisher
+		self.goal_point_pub = rospy.Publisher('move_base_simple/goal', PoseStamped, queue_size=1)
+		self.movebase_status_pub = rospy.Publisher('move_base_state', Int8, queue_size=1)
+		#ROS subscriber
+		self.move_base_status_sub = rospy.Subscriber('move_base/result', GoalStatus, self.GoalStatusArrayCallback, queue_size=1)
+		self.map_sub = rospy.Subscriber('map', OccupancyGrid, self.mapCallback)
+		self.point_sub = rospy.Subscriber('my_pose', Pose2D, self.moveCallback)
+		time.sleep(1)
+
+	def moveCallback(self,point_):
+		self.point = [point_.x, point_.y, math.sin(point_.theta/2), math.cos(point_.theta/2)]
+		#self.movebase_status=0
+		print("movebase_status::::%d" % self.movebase_status)
+		#moving
+		if self.movebase_status == 1:
+			return
+		#stop
+		elif self.movebase_status == 3:
+			self.pose = PoseStamped()
+			self.pose.pose.position.x = self.point[0]
+			self.pose.pose.position.y = self.point[1]
+			self.pose.pose.position.z = 0.0
+			#quat = tf.transformations.quaternion_from_euler(0.0, 0.0, 0.0)
+			self.pose.pose.orientation.x = 0
+			self.pose.pose.orientation.y = 0
+			self.pose.pose.orientation.z = self.point[2]
+			self.pose.pose.orientation.w = self.point[3]
+			self.pose.header.frame_id = self.my_bot_id #change
+			self.pose.header.stamp = rospy.Time.now()
+			self.goal_point_pub.publish(self.pose)
+			self.movebase_status = 1
+			return
+		elif self.movebase_status == 4:
+			return
+
+	def GoalStatusArrayCallback(self, data):
+		#print("movebase_status:%d" % self.movebase_status)
+		#status=Int8()
+		#status.data=self.movebase_status
+		#self.movebase_status_pub.publish(status)
+		#if self.movebase_status!=2:
+		#status_id = 0;
+		#uint8 PENDING         = 0  
+		#uint8 ACTIVE          = 1 
+		#uint8 PREEMPTED       = 2
+		#uint8 SUCCEEDED       = 3
+		#uint8 ABORTED         = 4
+		#uint8 REJECTED        = 5
+		#uint8 PREEMPTING      = 6
+		#uint8 RECALLING       = 7
+		#uint8 RECALLED        = 8
+		#uint8 LOST            = 9
+		if len(data.status.status) != 0:
+			self.movebase_status = data.status.status
+
+
+
+	def mapCallback(self, map_info):
+		self.my_bot_id = map_info.header.frame_id
+
+if __name__=="__main__":
+	rospy.init_node("move_burger")
+	OnigiriRun()
+	rospy.spin()
